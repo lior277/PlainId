7e688f4b4788ba8e1faf7e34aa584963f4aa1224
orweitzer <orw@connecteam.com>
2024-01-01 11:14:44 +0200

    Fixes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Pymobitest
 
-use python 3.9 or 3.10
+use python 3.9 or 3.10 or higher
 
 - git clone [git@github.com:Connecteam/automation-interview-exec.git] - url :https://github.com/Connecteam/automation-interview-exec
-- cd qa-automation-exercise
-- pip install -r requirements.txt
+- cd automation-interview-exec
+- pip3 install -r requirements.txt
 - pytest
--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(autouse=True)
 def chrome_web_driver_base(request):
     options = webdriver.ChromeOptions()
-    chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    downloaded_binary_path = ChromeDriverManager().install()
+    service = Service(executable_path=downloaded_binary_path)
+    chrome_driver = webdriver.Chrome(
+        service=service, options=options
+    )
     chrome_driver.delete_all_cookies()
     chrome_driver.maximize_window()
     yield chrome_driver
