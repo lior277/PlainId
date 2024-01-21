b135157ce604729e902827f6e73030338596744a
orweitzer <orw@connecteam.com>
2024-01-21 07:54:39 +0200

    Fixes

--- tests/test_submit_cv.py
+++ tests/test_submit_cv.py
@@ -1,10 +1,10 @@
 import time
 
 import pytest
-
+from selenium import webdriver
 
 @pytest.mark.usefixtures("chrome_web_driver_base")
 class TestSubmitCv:
-    def test_submit_cv(self, chrome_web_driver_base):
+    def test_submit_cv(self, chrome_web_driver_base: webdriver.Chrome):
         chrome_web_driver_base.get("https://connecteam.com/")
         time.sleep(5)
