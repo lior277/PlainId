4b38a38f985f29488088191a7fe136e0b9031674
orweitzer <orw@connecteam.com>
2025-01-19 19:32:46 +0200

    fixes

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# Pymobitest
+# QA Automation home assignment
 
-use python 3.9 or 3.10 or higher
+This repo is a skeleton project to help set up the selenium webdriver. you can choose to use it or not.
 
+If you chose not to use it you can use your one code for initializing the webdriver.
+Please note that this repo works on windows and linux machines.
+
+Perform the following steps:
+- Install python 3.9 or higher
 - git clone [git@github.com:Connecteam/automation-interview-exec.git] - url :https://github.com/Connecteam/automation-interview-exec
 - cd automation-interview-exec
 - pip3 install -r requirements.txt
-- pytest
+- run pytest
+
+After you finish the project setup please refer to instructions files for the task :)
--- conftest.py
+++ conftest.py
@@ -1,18 +1,29 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(autouse=True)
 def chrome_web_driver_base(request):
+    chrome_web_driver: WebDriver
     options = webdriver.ChromeOptions()
-    downloaded_binary_path = ChromeDriverManager().install()
-    service = Service(executable_path=downloaded_binary_path)
-    chrome_driver = webdriver.Chrome(
-        service=service, options=options
-    )
-    chrome_driver.delete_all_cookies()
-    chrome_driver.maximize_window()
-    yield chrome_driver
-    chrome_driver.close()
+    options.add_argument("--incognito")
+    options.add_argument("--allow-running-insecure-content")
+
+    try:
+        service = Service()
+        chrome_web_driver = webdriver.Chrome(
+            service=service, options=options
+        )
+    except Exception:
+        downloaded_binary_path = ChromeDriverManager().install()
+        service = Service(executable_path=downloaded_binary_path)
+        chrome_web_driver = webdriver.Chrome(
+            service=service, options=options
+        )
+
+    chrome_web_driver.maximize_window()
+    yield chrome_web_driver
+    chrome_web_driver.close()
