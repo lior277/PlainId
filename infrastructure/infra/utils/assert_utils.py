ec074c11b2e8444c7ce79bc3d66d226643a7902e
lior <lior277@gmail.com>
2025-04-02 08:43:00 +0300

    fix

--- infrastructure/infra/utils/assert_utils.py
+++ infrastructure/infra/utils/assert_utils.py
@@ -23,64 +23,11 @@ class CustomAssert:
             raise AssertionError(message)
 
     @staticmethod
-    def assert_false(condition, message=None):
-        if condition:
-            if message is None:
-                message = "Expected condition to be False, but it was True"
-            raise AssertionError(message)
-
-    @staticmethod
     def assert_equal(expected, actual, message=None):
         if expected != actual:
             if message is None:
                 message = f"Expected {expected}, but got {actual}"
             raise AssertionError(message)
 
-    @staticmethod
-    def assert_not_equal(expected, actual, message=None):
-        if expected == actual:
-            if message is None:
-                message = f"Expected {expected} and {actual} to be different"
-            raise AssertionError(message)
 
-    @staticmethod
-    def assert_in(expected, container, message=None):
-        if expected not in container:
-            if message is None:
-                message = f"Expected {expected} to be in {container}"
-            raise AssertionError(message)
 
-    @staticmethod
-    def assert_not_in(expected, container, message=None):
-        if expected in container:
-            if message is None:
-                message = f"Expected {expected} not to be in {container}"
-            raise AssertionError(message)
-
-    @staticmethod
-    def assert_greater(first, second, message=None):
-        if not first > second:
-            if message is None:
-                message = f"Expected {first} to be greater than {second}"
-            raise AssertionError(message)
-
-    @staticmethod
-    def assert_greater_equal(first, second, message=None):
-        if not first >= second:
-            if message is None:
-                message = "Expected {first} to be greater than or equal to {second}"
-            raise AssertionError(message)
-
-    @staticmethod
-    def assert_less(first, second, message=None):
-        if not first < second:
-            if message is None:
-                message = "Expected {first} to be less than {second}"
-            raise AssertionError(message)
-
-    @staticmethod
-    def assert_less_equal(first, second, message=None):
-        if not first <= second:
-            if message is None:
-                message = "Expected {first} to be less than or equal to {second}"
-            raise AssertionError(message)
